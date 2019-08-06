9bbc459684c3aaa56d965298143bf09f36106652
Seth Bernard <sethbernard7@gmail.com>
2019-08-06 13:32:14 -0400

    Add functionality to logout page

--- client/src/components/LogoutPage.js
+++ client/src/components/LogoutPage.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import fb from '../firebase/config';
+import 'firebase/auth';
 
-const LogoutPage = () => {
-  return (
-    <div>
-      <h1>Bye, from the Logout Page!</h1>
-    </div>
-  );
-};
+class LogoutPage extends Component {
+  authStateChanged = () => {
+    fb.auth().onAuthStateChanged(firebaseUser => {
+      if (firebaseUser) {
+        console.log(firebaseUser);
+      } else {
+        console.log('NOT LOGGED IN');
+      }
+    });
+  };
+
+  logOut = () => {
+    fb.auth().signOut();
+  };
+
+  componentDidMount() {
+    this.authStateChanged();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Logout Page!</h1>
+        <Button color="red" size="large" onClick={this.logOut}>
+          Log Out
+        </Button>
+      </div>
+    );
+  }
+}
 
 export default LogoutPage;
