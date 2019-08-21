a233aa2687e5f9c2dd41e7e14f929c0a5d41bbde
Seth Bernard <sethbernard7@gmail.com>
2019-08-21 17:48:46 -0400

    Load more recipe cards by clicking button on home page

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,26 +2,28 @@ import React, { Component } from 'react';
 import RecipeSearch from '../RecipeSearch';
 import RecipeCards from '../RecipeCards';
 import { PATH, APP_ID, API_KEY } from '../../utils/edamam-api-info';
+import { Grid, Button } from 'semantic-ui-react';
 import axios from 'axios';
-import firebase from '../../firebase/config';
+// import firebase from '../../firebase/config';
 
 class HomePage extends Component {
   state = {
     recipeSearchTerm: 'pulled+pork',
     recipes: [],
+    toParameter: 6,
     error: null
   };
 
   componentDidMount() {
     this.getData();
-    console.log(firebase.auth().currentUser);
+    // console.log(firebase.auth().currentUser);
   }
 
   getData = async () => {
     try {
-      const { recipeSearchTerm } = this.state;
+      const { recipeSearchTerm, toParameter } = this.state;
       const response = await axios.get(
-        `${PATH}?q=${recipeSearchTerm}&app_id=${APP_ID}&app_key=${API_KEY}&to=12`
+        `${PATH}?q=${recipeSearchTerm}&app_id=${APP_ID}&app_key=${API_KEY}&to=${toParameter}`
       );
       const recipes = response.data.hits;
       await this.setState(prevState => ({
@@ -42,6 +44,14 @@ class HomePage extends Component {
     this.getData();
   };
 
+  handleToParameter = async () => {
+    await this.setState(prevState => ({
+      ...prevState,
+      toParameter: this.state.toParameter + 6
+    }));
+    await this.getData();
+  };
+
   render() {
     return (
       <div>
@@ -50,6 +60,17 @@ class HomePage extends Component {
           onSubmit={this.handleSubmit}
         />
         <RecipeCards recipes={this.state.recipes} />
+
+        <Grid centered container>
+          <Button
+            size="large"
+            color="blue"
+            style={{ margin: '2rem 0 1rem 0' }}
+            onClick={this.handleToParameter}
+          >
+            Load more recipes
+          </Button>
+        </Grid>
       </div>
     );
   }
