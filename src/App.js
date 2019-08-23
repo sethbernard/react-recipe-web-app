8511c4a6f88965a808b89c84b6613dfae2bd0195
Seth Bernard <sethbernard7@gmail.com>
2019-08-23 17:23:39 -0400

    Add user authentication status to state on mount

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import firebase from './firebase/config';
+import 'firebase/auth';
 import Navbar from './components/Navbar';
 import HomePage from './components/pages/HomePage';
 import SavedRecipesPage from './components/pages/SavedRecipesPage';
@@ -11,11 +13,28 @@ import LogoutPage from './components/pages/LogoutPage';
 import Recipe from './components/Recipe';
 
 class App extends Component {
+  state = {
+    userAuthenticated: false
+  };
+
+  componentDidMount = () => {
+    // Check to see if a user is authenticated
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ ...this.state, userAuthenticated: true });
+        console.log(user);
+      } else {
+        this.setState({ userAuthenticated: false });
+        console.log('No user is signed in');
+      }
+    });
+  };
+
   render() {
     return (
       <div>
         <BrowserRouter>
-          <Navbar />
+          <Navbar auth={this.state.userAuthenticated} />
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route path="/saved-recipes" component={SavedRecipesPage} />
