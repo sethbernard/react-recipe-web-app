1b691751278ca508357fdfeb745cc4445065bcb4
Seth Bernard <sethbernard7@gmail.com>
2019-08-30 20:25:37 -0400

    Space 'view recipe' link and trash icon evenly on card

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Image, Button } from 'semantic-ui-react';
+import { Card, Image, Button, Icon } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
 
 const RecipeCard = ({ ...props }) => {
@@ -10,17 +10,23 @@ const RecipeCard = ({ ...props }) => {
         <Card.Header>{props.header}</Card.Header>
         <Card.Meta style={{ marginTop: '0.5rem' }}>{props.meta}</Card.Meta>
       </Card.Content>
-      <Card.Content
-        style={{ display: 'flex', justifyContent: 'space-between' }}
-      >
-        <span>{props.link}</span>
-        <span onClick={props.deleteRecipe}>
-          <Button style={{ background: 'none', border: 'none', padding: '0' }}>
-            {props.location.pathname === '/saved-recipes'
-              ? 'Delete Recipe'
-              : null}
-          </Button>
-        </span>
+      <Card.Content>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <span>{props.link}</span>
+          <span onClick={props.deleteRecipe}>
+            <Button
+              style={{
+                background: 'none',
+                border: 'none',
+                padding: '0'
+              }}
+            >
+              {props.location.pathname === '/saved-recipes' ? (
+                <Icon name="trash alternate" />
+              ) : null}
+            </Button>
+          </span>
+        </div>
       </Card.Content>
     </Card>
   );
