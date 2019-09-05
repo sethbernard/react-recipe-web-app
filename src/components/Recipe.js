55ec69a0ca8b17ee3ef65d0968d6c11e5d0a94ba
Seth Bernard <sethbernard7@gmail.com>
2019-09-05 18:07:03 -0400

    Refactor to reduce size of file

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,207 +1,64 @@
 import React, { useEffect } from 'react';
+import RecipeHeader from './RecipeHeader';
+import TimeServingsCalories from './TimeServingsCalories';
+import RecipeImage from './RecipeImage';
+import HealthLabels from './HealthLabels';
+import IngredientsList from './IngredientsList';
+import DietLabels from './DietLabels';
+import CautionLabels from './pages/CautionLabels';
+import ExternalLink from './ExternalLink';
+import SaveRecipeButton from './SaveRecipeButton';
+import { Grid, Image } from 'semantic-ui-react';
 import firebase from '../firebase/config';
-import 'firebase/auth';
-import { Link } from 'react-router-dom';
-
-import {
-  Header,
-  Segment,
-  Grid,
-  Icon,
-  Image,
-  List,
-  Label,
-  Button
-} from 'semantic-ui-react';
 
 const Recipe = props => {
-  // console.log(props);
-
-  // Scroll to very top of window after recipe card link is clicked
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  // Destructure data being sent over from React Router Link
-  const {
-    id,
-    label,
-    source,
-    totalTime,
-    servings,
-    calories,
-    image,
-    ingredientLines,
-    dietLabels,
-    healthLabels,
-    cautions,
-    url
-  } = props.location.state;
-
-  const recipeData = props.location.state;
-
-  console.log(recipeData);
+  const recipeData = props.location.state; // store recipe data being transfered from react router
 
   // Save recipe id to database for logged in user
-  const handleSaveRecipe = recipeData => {
-    const db = firebase.firestore();
-    const user = firebase.auth().currentUser;
-
-    // Add a new document in collection
-    db.collection('userrecipes')
-      .doc(recipeData.id)
-      .set({
-        id,
-        saved: true,
-        userId: user.uid,
-        image,
-        label,
-        source,
-        totalTime,
-        servings,
-        calories,
-        ingredientLines,
-        dietLabels,
-        healthLabels,
-        cautions,
-        url
-      })
-      .then(() => {
-        console.log('Document successfully written!');
-      })
-      .catch(error => {
-        console.error('Error writing document: ', error);
-      });
+  const handleSaveRecipe = async recipeData => {
+    try {
+      const user = await firebase.auth().currentUser;
+      const db = await firebase.firestore();
+      await db
+        .collection('userrecipes')
+        .doc(recipeData.id) // Add a new document in collection
+        .set({
+          saved: true,
+          userId: user.uid,
+          ...recipeData
+        });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <Grid columns={2} stackable container>
       <Grid.Column size={12}>
-        <Header as="h1" style={{ margin: '2rem 0rem', textAlign: 'center' }}>
-          {label}
-        </Header>
-
-        {/* Time, Servings, Calories */}
-        <Segment.Group raised horizontal>
-          <Segment>
-            <Icon name="clock" />
-            {totalTime === 0 ? 'N/A' : `${totalTime} minutes`}
-          </Segment>
-          <Segment>
-            <Icon name="food" />
-            {servings === undefined ? 'N/A' : `${servings} servings`}
-          </Segment>
-          <Segment>
-            <Icon name="heart" color="red" />
-            {`${Math.round(calories)} calories`}
-          </Segment>
-        </Segment.Group>
-
-        {/* Health Labels */}
-        <Segment
-          raised
-          style={{ textAlign: 'center', marginTop: '3rem', padding: '2rem' }}
-        >
-          <Header as="h3">Health Labels</Header>
-          {healthLabels.map((healthLabel, i) => {
-            return (
-              <Label
-                key={i}
-                size="large"
-                color="green"
-                style={{ marginBottom: '4px' }}
-              >
-                {healthLabel}
-              </Label>
-            );
-          })}
-        </Segment>
+        <RecipeHeader header={recipeData.label} />
+        <TimeServingsCalories
+          totalTime={recipeData.totalTime}
+          servings={recipeData.servings}
+          calories={recipeData.calories}
+        />
+        <HealthLabels healthLabels={recipeData.healthLabels} />
       </Grid.Column>
 
-      {/* Recipe Image */}
-      <Grid.Column size={4} textAlign="center" style={{ marginTop: '3rem' }}>
-        <Image src={image} size="medium" spaced="left" rounded />
-      </Grid.Column>
+      <RecipeImage image={recipeData.image} />
 
-      {/* Ingredients List */}
       <Grid.Column>
-        <Header as="h3" textAlign="center">
-          Ingredients List
-        </Header>
-        <Segment raised style={{ minHeight: '287px', padding: '2rem' }}>
-          <List bulleted>
-            {ingredientLines.map((step, i) => {
-              return (
-                <List.Item key={i} style={{ marginBottom: '1rem' }}>
-                  {step}
-                </List.Item>
-              );
-            })}
-          </List>
-        </Segment>
-
-        {/* Recipe External Link */}
-        <Segment
-          style={{
-            margin: '2rem 0 2rem 0',
-            fontSize: '1.2rem',
-            textAlign: 'center'
-          }}
-          raised
-        >
-          View full recipe with directions
-          <a href={url} target="_blank" rel="noopener noreferrer">
-            {' '}
-            here
-          </a>
-        </Segment>
+        <IngredientsList ingredients={recipeData.ingredientLines} />
+        <ExternalLink url={recipeData.url} />
       </Grid.Column>
 
-      {/* Diet Labels */}
       <Grid.Column size={4} textAlign="center" style={{ marginTop: '2.5rem' }}>
-        <Segment raised style={{ padding: '2rem' }}>
-          <Header as="h3">Diet Labels</Header>
-          {dietLabels && dietLabels.length ? (
-            dietLabels.map((dietLabel, i) => {
-              return (
-                <Label key={i} size="large" color="blue">
-                  {dietLabel}
-                </Label>
-              );
-            })
-          ) : (
-            <Label size="large" color="blue">
-              No Diet Labels
-            </Label>
-          )}
-        </Segment>
-
-        {/* Caution Labels */}
-        <Segment raised style={{ marginTop: '3rem', padding: '2rem' }}>
-          <Header as="h3">Cautions</Header>
-          {cautions && cautions.length ? (
-            cautions.map((caution, i) => {
-              return (
-                <Label key={i} size="large" color="red">
-                  {caution}
-                </Label>
-              );
-            })
-          ) : (
-            <Label size="large" color="red">
-              No Cautions
-            </Label>
-          )}
-        </Segment>
-
-        {/* Save Recipe Button */}
-        <Button
-          size="large"
-          style={{ margin: '2rem 0 2rem 0' }}
-          onClick={() => handleSaveRecipe(recipeData)}
-        >
-          <Link to="/saved-recipes">Save Recipe</Link>
-        </Button>
+        <DietLabels dietLabels={recipeData.dietLabels} />
+        <CautionLabels cautions={recipeData.cautions} />
+        <SaveRecipeButton handleClick={() => handleSaveRecipe(recipeData)} />
       </Grid.Column>
     </Grid>
   );
