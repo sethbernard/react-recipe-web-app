bd0172515e77ad40cc903e71cfaa3aa983fe9b82
Seth Bernard <sethbernard7@gmail.com>
2019-09-12 12:57:06 -0400

    Make all cards the same size

--- src/components/RecipeCards.js
+++ src/components/RecipeCards.js
@@ -6,59 +6,61 @@ import { Link } from 'react-router-dom';
 const RecipeCards = ({ recipes }) => {
   return (
     <Grid columns={3} stackable centered container>
-      {recipes.map((card, index) => {
-        const {
-          label,
-          image,
-          source,
-          uri,
-          url,
-          dietLabels,
-          ingredientLines,
-          calories,
-          totalTime,
-          healthLabels,
-          cautions
-        } = card.recipe;
+      <Grid.Row stretched>
+        {recipes.map((card, index) => {
+          const {
+            label,
+            image,
+            source,
+            uri,
+            url,
+            dietLabels,
+            ingredientLines,
+            calories,
+            totalTime,
+            healthLabels,
+            cautions
+          } = card.recipe;
 
-        const id = uri.replace(
-          'http://www.edamam.com/ontologies/edamam.owl#recipe_', // Make custom ID
-          ''
-        );
-        return (
-          <Grid.Column key={index} width={5}>
-            <RecipeCard
-              image={image}
-              header={label}
-              meta={source}
-              id={id}
-              link={
-                <Link
-                  to={{
-                    pathname: `/recipe/${id}`,
-                    state: {
-                      id,
-                      label,
-                      image,
-                      source,
-                      url,
-                      servings: card.recipe.yield, // had to use servings as a key because yield is a reserved keyword
-                      dietLabels,
-                      ingredientLines,
-                      calories,
-                      totalTime,
-                      healthLabels,
-                      cautions
-                    }
-                  }}
-                >
-                  View Recipe
-                </Link>
-              }
-            />
-          </Grid.Column>
-        );
-      })}
+          const id = uri.replace(
+            'http://www.edamam.com/ontologies/edamam.owl#recipe_', // Make custom ID
+            ''
+          );
+          return (
+            <Grid.Column key={index} width={5}>
+              <RecipeCard
+                image={image}
+                header={label}
+                meta={source}
+                id={id}
+                link={
+                  <Link
+                    to={{
+                      pathname: `/recipe/${id}`,
+                      state: {
+                        id,
+                        label,
+                        image,
+                        source,
+                        url,
+                        servings: card.recipe.yield, // had to use servings as a key because yield is a reserved keyword
+                        dietLabels,
+                        ingredientLines,
+                        calories,
+                        totalTime,
+                        healthLabels,
+                        cautions
+                      }
+                    }}
+                  >
+                    View Recipe
+                  </Link>
+                }
+              />
+            </Grid.Column>
+          );
+        })}
+      </Grid.Row>
     </Grid>
   );
 };
